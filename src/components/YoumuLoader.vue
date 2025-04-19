756c7d90748aa68352855b779418afba1814ec8c
uzki69 <uzakichan11224@gmail.com>
2025-04-19 00:23:28 -0300

    added audio option

--- src/components/YoumuLoader.vue
+++ src/components/YoumuLoader.vue
@@ -6,7 +6,8 @@ const hostnameRef = ref('')
 const imageSrcRef = ref('');
 const videoNameRef = ref('');
 const tokenRef = ref('');
-const buttonStatus = ref('Download Video');
+const buttonStatusRef = ref('Download Video');
+const isAudioRef = ref('');
 
 function getVideoId(url) {
     const regExp = /^.*(youtu.be\/|v\/|u\/\w\/|embed\/|watch\?v=|&v=)([^#&?]*).*/;
@@ -36,44 +37,45 @@ document.addEventListener('DOMContentLoaded', async () => {
     const videoId = getVideoId(ytObj.url);
 
     const title =  ytObj.yttab.title.trim();
-    const cookie = await browser.storage.local.get(["hostname", "token"]);
+    const cookie = await browser.storage.local.get(["hostname", "token", "isAudio"]);
     imageSrcRef.value = `https://i.ytimg.com/vi/${videoId}/hqdefault.jpg`
     videoNameRef.value = title ? title : "Not available";
     tokenRef.value = cookie.token ? cookie.token : "";
+    isAudioRef.value = cookie.isAudio ? cookie.isAudio : false;
     hostnameRef.value = cookie.hostname ? cookie.hostname : "";
 })
 
 async function downloadVideo() {
     const btn = document.querySelector("#download-button");
     const url =  (await getCurrentYTUrl()).url;
-    buttonStatus.value = "Downloading..."
+    buttonStatusRef.value = "Downloading..."
     btn.style.backgroundColor = "#777"
     btn.style.borderColor = "#777"
     const lastUrl = (await browser.storage.local.get("lastVideo")).lastVideo
     if (lastUrl === url) {
-        buttonStatus.value = "Video Already Downloaded..."
+        buttonStatusRef.value = "Video Already Downloaded..."
         btn.style.backgroundColor = "yellow"
         btn.style.borderColor = "#777"
         return
     }
-    browser.storage.local.set({ token: tokenRef.value, hostname: hostnameRef.value })
+    browser.storage.local.set({ token: tokenRef.value, hostname: hostnameRef.value, isAudio: isAudioRef.value})
     axios.get(`${hostnameRef.value}youmu/download`, {
         params: {
             video: url,
-            audio: false
+            audio: isAudioRef.value ? true : false
         },
         headers: {
             Authorization: `MediaBrowser Token="${tokenRef.value}"`
         }
     })
         .then(() => {
-            buttonStatus.value = "Video Downloaded!";
+            buttonStatusRef.value = "Video Downloaded!";
             btn.style.backgroundColor = "green"
             btn.style.borderColor = "green"
             browser.storage.local.set({ lastVideo:  url})
         })
         .catch(() => {
-            buttonStatus.value = "Something Wrong?";
+            buttonStatusRef.value = "Something Wrong?";
             btn.style.backgroundColor = "red"
             btn.style.borderColor = "red"
         })
@@ -91,10 +93,14 @@ async function downloadVideo() {
             <input id="passwd" type="password" v-model="tokenRef">
             <p>{{ tokenRef.slice(0, 3) }}...{{ tokenRef.slice(-3) }}</p>
         </div>
+        <div>
+            <label for="audio-box">Audio: </label>
+            <input type="checkbox" name="" id="audio-box" v-model="isAudioRef">
+        </div>
         <div id="download-container">
             <img :src="imageSrcRef" alt="Video thumb" id="video-image">
             <p id="video-name">{{ videoNameRef }}</p>
-            <button id="download-button" @click="downloadVideo()">{{ buttonStatus }}</button>
+            <button id="download-button" @click="downloadVideo()">{{ buttonStatusRef }}</button>
         </div>
     </div>
 </template>
