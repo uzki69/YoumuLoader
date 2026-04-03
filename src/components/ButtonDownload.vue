2cdb6798f01a99031d9c78b35beeb9660d654efd
uzki69 <uzakichan11224@gmail.com>
2026-04-03 14:40:49 -0300

    styling

---
+++ src/components/ButtonDownload.vue
@@ -0,0 +1,36 @@
+<script lang="ts" setup>
+import type { ButtonHTMLAttributes } from 'vue';
+
+interface Props extends /* @vue-ignore */ ButtonHTMLAttributes {}
+defineProps<Props>();
+
+</script>
+
+<template>
+    <span>
+        <button class="button-download" v-bind="$attrs">
+            <slot></slot>
+        </button>
+    </span>
+</template>
+
+<style scoped>
+    .button-download {
+        background: hsl(138, 67%, 47%);
+        color: #f0f0f0;
+        border: 1px solid #181818;
+        padding: 0.7em;
+        text-shadow: 1px 1px 2px #181818;
+    }
+    
+    .button-download:hover {
+        background: hsl(138, 29%, 57%);
+    }
+
+    .button-download:active {
+        background: hsl(138, 69%, 30%);
+    }
+
+
+
+</style>
---
+++ src/components/TextInput.vue
@@ -0,0 +1,42 @@
+<script lang="ts" setup>
+import type { InputHTMLAttributes } from 'vue';
+
+interface TextInput extends /* @vue-ignore */ InputHTMLAttributes{}
+defineProps<TextInput>();
+
+</script>
+<template>
+    <span>
+        <div class="container">
+            <label :for="`textinput_${$attrs.name as string}`" class="label">
+                <slot></slot>
+            </label>
+            <input v-bind="$attrs" class="input"  :id="`textinput_${$attrs.name as string}`" type="text">
+        </div>
+    </span>
+    </template>
+<style scoped>
+.container {
+    display: flex;
+    padding: 8px;
+    column-gap: 16px;
+    align-items: center;
+}
+
+.label {
+    color: #fefefe;
+}
+
+.input {
+    background-color: #4f4f4f;
+    color: #fefefe;
+    outline: none;
+    border: 0;
+    font-size: larger;
+    padding: 0.3em 1ch;
+    flex: 1;
+}
+.input:focus {
+    box-shadow: 0px 1px #fefefe
+}
+</style>
